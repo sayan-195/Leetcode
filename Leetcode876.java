e4f0e1a188b7b0daa9067b757d0cc624f510931c
sayan-195 <sayanghosh0195@gmail.com>
2025-06-26 20:58:21 +0530

    Find the Middle of The Linkedlist

---
+++ Leetcode876.java
@@ -0,0 +1,49 @@
+public class Leetcode876 {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    static Node findMiddleElement(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+        Node ans = findMiddleElement(a);
+        System.out.println(ans.data);
+    }
+
+}
