6306e396efd9e330c9e36523da4e91ce7805bef0
sayan-195 <sayanghosh0195@gmail.com>
2025-06-30 20:27:18 +0530

    Odd Even Linkedlist

---
+++ Leetcode328.java
@@ -0,0 +1,57 @@
+public class Leetcode328 {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    static Node oddEvenLinkedlist(Node head) {
+        Node odd = head;
+        Node even = head.next;
+        Node evenhead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenhead;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        Node h = new Node(8);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        f.next = g;
+        g.next = h;
+
+        Node ans = oddEvenLinkedlist(a);
+        display(ans);
+    }
+}
